9db32434c14d4e8b9d347d032b962caf968ebf85
wulawulu <w2473698581@163.com>
2025-04-07 21:04:01 +0800

    feature: build a much better describe function

---
+++ src/backend/describe.rs
@@ -0,0 +1,175 @@
+use std::sync::Arc;
+
+use arrow::datatypes::{DataType, Field};
+use datafusion::prelude::{DataFrame, array_length, case, cast, col, is_null, length, lit};
+
+use datafusion::functions_aggregate::expr_fn::{avg, count, max, median, min, stddev, sum};
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub enum DescribeMethod {
+    Total,
+    NullTotal,
+    Mean,
+    Stddev,
+    Min,
+    Max,
+    Median,
+    Percentile(u8),
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct DataFrameDescriber {
+    original: DataFrame,
+    transformed: DataFrame,
+    methods: Vec<DescribeMethod>,
+}
+
+#[allow(dead_code)]
+impl DataFrameDescriber {
+    pub fn try_new(df: DataFrame) -> anyhow::Result<Self> {
+        let fields = df.schema().fields().iter();
+        let expressions = fields
+            .map(|field| {
+                let dt = field.data_type();
+                let expr = match dt {
+                    dt if dt.is_temporal() => cast(col(field.name()), DataType::Float64),
+                    dt if dt.is_numeric() => col(field.name()),
+                    DataType::List(_) | DataType::LargeList(_) => array_length(col(field.name())),
+                    _ => length(cast(col(field.name()), DataType::Utf8)),
+                };
+                expr.alias(field.name())
+            })
+            .collect();
+
+        let transformed = df.clone().select(expressions)?;
+
+        Ok(Self {
+            original: df,
+            transformed,
+            methods: vec![
+                DescribeMethod::Total,
+                DescribeMethod::NullTotal,
+                DescribeMethod::Mean,
+                DescribeMethod::Stddev,
+                DescribeMethod::Min,
+                DescribeMethod::Max,
+                DescribeMethod::Median,
+            ],
+        })
+    }
+
+    pub async fn describe(&self) -> anyhow::Result<DataFrame> {
+        let df = self.do_describe().await?;
+        self.cast_down(df)
+    }
+
+    async fn do_describe(&self) -> anyhow::Result<DataFrame> {
+        let df: Option<DataFrame> = self.methods.iter().fold(None, |acc, method| {
+            let df = self.transformed.clone();
+            let stat_df = match method {
+                DescribeMethod::Total => total(df).unwrap(),
+                DescribeMethod::NullTotal => null_total(df).unwrap(),
+                DescribeMethod::Mean => mean(df).unwrap(),
+                DescribeMethod::Stddev => std_div(df).unwrap(),
+                DescribeMethod::Min => minimum(df).unwrap(),
+                DescribeMethod::Max => maximum(df).unwrap(),
+                DescribeMethod::Median => med(df).unwrap(),
+                DescribeMethod::Percentile(_) => todo!(),
+            };
+
+            // add a new column to the beginning of the dataframe
+            let mut select_expr = vec![lit(method.to_string()).alias("describe")];
+            select_expr.extend(stat_df.schema().fields().iter().map(|f| col(f.name())));
+
+            let stat_df = stat_df.select(select_expr).unwrap();
+
+            match acc {
+                Some(acc) => Some(acc.union(stat_df).unwrap()),
+                None => Some(stat_df),
+            }
+        });
+        df.ok_or_else(|| anyhow::anyhow!("No statistics found"))
+    }
+
+    fn cast_down(&self, df: DataFrame) -> anyhow::Result<DataFrame> {
+        let describe = Arc::new(Field::new("describe", DataType::Utf8, false));
+        let mut fields = vec![&describe];
+        fields.extend(self.original.schema().fields().iter());
+
+        let expressions = fields
+            .into_iter()
+            .map(|field| {
+                let dt = field.data_type();
+                let expr = match dt {
+                    dt if dt.is_temporal() => cast(col(field.name()), dt.clone()),
+                    DataType::List(_) | DataType::LargeList(_) => {
+                        cast(col(field.name()), DataType::Int32)
+                    }
+                    _ => col(field.name()),
+                };
+                expr.alias(field.name())
+            })
+            .collect();
+
+        Ok(df
+            .select(expressions)?
+            .sort(vec![col("describe").sort(true, false)])?)
+    }
+}
+
+impl std::fmt::Display for DescribeMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DescribeMethod::Total => write!(f, "total"),
+            DescribeMethod::NullTotal => write!(f, "null_total"),
+            DescribeMethod::Mean => write!(f, "mean"),
+            DescribeMethod::Stddev => write!(f, "stddev"),
+            DescribeMethod::Min => write!(f, "min"),
+            DescribeMethod::Max => write!(f, "max"),
+            DescribeMethod::Median => write!(f, "median"),
+            DescribeMethod::Percentile(p) => write!(f, "percentile_{}", p),
+        }
+    }
+}
+
+macro_rules! describe_method {
+    ($name:ident,$method:ident) => {
+        fn $name(df: DataFrame) -> anyhow::Result<DataFrame> {
+            let fields = df.schema().fields().iter();
+            let ret = df.clone().aggregate(
+                vec![],
+                fields
+                    .filter(|f| f.data_type().is_numeric())
+                    .map(|f| $method(col(f.name())).alias(f.name()))
+                    .collect::<Vec<_>>(),
+            )?;
+            Ok(ret)
+        }
+    };
+}
+
+describe_method!(total, count);
+describe_method!(mean, avg);
+describe_method!(std_div, stddev);
+describe_method!(minimum, min);
+describe_method!(maximum, max);
+describe_method!(med, median);
+
+fn null_total(df: DataFrame) -> anyhow::Result<DataFrame> {
+    let fields = df.schema().fields().iter();
+    let ret = df.clone().aggregate(
+        vec![],
+        fields
+            .map(|f| {
+                sum(case(is_null(col(f.name())))
+                    .when(lit(true), lit(1))
+                    .otherwise(lit(0))
+                    .unwrap())
+                .alias(f.name())
+            })
+            .collect::<Vec<_>>(),
+    )?;
+    Ok(ret)
+}
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,3 +1,112 @@
-pub mod datafusion;
+pub mod describe;
 
-pub use datafusion::DatafusionBackend;
+use std::ops::Deref;
+
+use crate::{Backend, ConnectOpts, DatasetConn, ReplDisplay};
+use anyhow::Result;
+use arrow::{array::RecordBatch, util::pretty::pretty_format_batches};
+use datafusion::prelude::{
+    CsvReadOptions, DataFrame, NdJsonReadOptions, SessionConfig, SessionContext,
+};
+use describe::DataFrameDescriber;
+
+pub struct DatafusionBackend(SessionContext);
+
+impl DatafusionBackend {
+    pub fn new() -> Self {
+        let mut config = SessionConfig::new();
+        config.options_mut().catalog.information_schema = true;
+
+        Self(SessionContext::new_with_config(config))
+    }
+}
+
+impl Backend for DatafusionBackend {
+    async fn connect(&mut self, opts: &ConnectOpts) -> Result<()> {
+        match &opts.conn {
+            DatasetConn::Parquet(path) => {
+                self.register_parquet(&opts.name, path, Default::default())
+                    .await?;
+            }
+            DatasetConn::Postgres(_) => todo!(),
+            DatasetConn::Csv(file_opts) => {
+                let options = CsvReadOptions {
+                    file_extension: &file_opts.extension,
+                    file_compression_type: file_opts.compression,
+                    ..Default::default()
+                };
+                self.register_csv(&opts.name, &file_opts.filename, options)
+                    .await?;
+            }
+            DatasetConn::Json(file_opts) => {
+                let options = NdJsonReadOptions {
+                    file_extension: &file_opts.extension,
+                    file_compression_type: file_opts.compression,
+                    ..Default::default()
+                };
+                self.register_json(&opts.name, &file_opts.filename, options)
+                    .await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn describe(&self, name: &str) -> anyhow::Result<impl ReplDisplay> {
+        let df = self.0.sql(&format!("SELECT * FROM {}", name)).await?;
+        let describe_df = DataFrameDescriber::try_new(df)?;
+        let describe_df = describe_df.describe().await?;
+        Ok(describe_df)
+    }
+
+    async fn head(&self, name: &str, size: usize) -> anyhow::Result<impl ReplDisplay> {
+        let df = self
+            .0
+            .sql(&format!("SELECT * FROM {} LIMIT {}", name, size))
+            .await?;
+        Ok(df)
+    }
+
+    async fn list(&self) -> anyhow::Result<impl ReplDisplay> {
+        let df = self.0.sql("SELECT table_name, table_type FROM information_schema.tables WHERE table_schema = 'public'").await?;
+        Ok(df)
+    }
+
+    async fn schema(&self, name: &str) -> anyhow::Result<impl ReplDisplay> {
+        let df = self.0.sql(&format!("DESCRIBE {}", name)).await?;
+        Ok(df)
+    }
+
+    async fn sql(&self, sql: &str) -> anyhow::Result<impl ReplDisplay> {
+        let df = self.0.sql(sql).await?;
+        Ok(df)
+    }
+}
+
+impl Default for DatafusionBackend {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Deref for DatafusionBackend {
+    type Target = SessionContext;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl ReplDisplay for DataFrame {
+    async fn display(self) -> anyhow::Result<String> {
+        let results = self.collect().await?;
+        let data = pretty_format_batches(&results)?;
+        Ok(data.to_string())
+    }
+}
+
+impl ReplDisplay for RecordBatch {
+    async fn display(self) -> anyhow::Result<String> {
+        let data = pretty_format_batches(&[self])?;
+        Ok(data.to_string())
+    }
+}
